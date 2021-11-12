d9ed5b655a702b089ffc48f88f436863ddb590e8
JuniorCoderChris <90716291+JuniorCoderChris@users.noreply.github.com>
2021-11-12 15:08:59 -0500

    Add files via upload

---
+++ TTT.js
@@ -0,0 +1,65 @@
+var p1Score = 0;
+var p2Score = 0;
+var tie = 0;
+var turn = 1;
+
+function checkWinX(){
+    if ($(".row-1 .box.x").length === 3
+    || $(".row-2 .box.x").length === 3
+    || $(".row-3 .box.x").length === 3
+    || $(".col1.x").length === 3
+    || $(".col2.x").length === 3
+    || $(".col3.x").length === 3
+    || $("#1").hasClass("x")
+    && $("#5").hasClass("x")
+    && $("#9").hasClass("x")
+    || $("#3").hasClass("x")
+    && $("#5").hasClass("x")
+    && $("#7").hasClass("x")){
+        return true
+    }
+}
+function checkWinO(){
+    if ($(".row-1 .box.o").length === 3
+    || $(".row-2 .box.o").length === 3
+    || $(".row-3 .box.o").length === 3
+    || $(".col1.o").length === 3
+    || $(".col2.o").length === 3
+    || $(".col3.o").length === 3
+    || $("#1").hasClass("o")
+    && $("#5").hasClass("o")
+    && $("#9").hasClass("o")
+    || $("#3").hasClass("o")
+    && $("#5").hasClass("o")
+    && $("#7").hasClass("o")){
+        return true
+    }
+}
+
+$(".box").click(function(){
+    if (turn === 1){
+        $(this).text("x")
+        $(this).addClass("x")
+        turn = 2;
+        $("#turn").text(2)
+        if(checkWinX()){
+            p1Score++
+            $("#p1Score").text(p1Score)
+            clear()
+        }
+        else {
+            CheckTie()
+        }
+    }
+    else
+    {
+        $(this).text("o")
+        $(this).addClass("o")
+        turn = 1;
+        $("#turn").text(1)
+        if(checkWinO()){
+            p2Score++
+            $("#p2Score").text(p2Score)
+        }
+    }
+})
